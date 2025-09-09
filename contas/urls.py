142c28d4dedc017ae59fcec0fb5e26d821c24aa8
“Lara <laragazotti@gmail.com>
2025-09-09 12:02:35 -0300

    commit

--- contas/urls.py
+++ contas/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('criar_conta/', views.criar_conta, name='criar_conta'),
     path('exibir_conta/', views.exibir_conta, name='exibir_conta'),
     path('excluir_conta/', views.excluir_conta, name='excluir_conta'),
+    path('calculos/', views.calculos, name='calculos'),
+    path('criar_categorias/', views.criar_categorias, name='criar_categorias'),
+    path('servicos/', views.servicos, name='servicos'),
 ]
